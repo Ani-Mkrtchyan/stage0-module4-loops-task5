2141d57ab8263294c4fe81a2651f577d9975d48f
Ani <ani.mkrtchyan1982@gmail.com>
2023-03-17 01:07:21 +0400

    using for loop( triangle change)

--- src/main/java/school/mjc/stage0/loops/task5/Triangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Triangle.java
@@ -6,7 +6,7 @@ public class Triangle {
             for (int j = 1; j <= i; j++) {
                 System.out.print("8");
             }
-            System.out.println(" ");
+            System.out.println();
 
         }
     }
